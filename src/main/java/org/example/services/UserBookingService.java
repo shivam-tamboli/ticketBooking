13db1b73d91a00e8b30267cbcb09a86cdf4be887
Shivam <shivamtamboli66@gmail.com>
2026-02-07 14:27:31 +0530

    bussiness logic for user signup and login

---
+++ src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,48 @@
+package org.example.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entity.User;
+
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public class UserBookingService {
+
+    private User user;
+
+    private List<User> userList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String USERS_PATH = "lib/src/main/java/org.example/localDb/users.json";
+
+    public UserBookingService(User user) throws IOException {
+
+        this.user = user;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>(){});
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try {
+            userList.add((user1));
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+}
