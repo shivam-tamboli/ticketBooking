86aef6c33f6547de921fac9b1af14c286abf2f8c
Shivam <shivamtamboli66@gmail.com>
2026-02-07 14:40:31 +0530

    convert plain password to hashPassword and check enter password

---
+++ src/main/java/org/example/util/userServiceUtil.java
@@ -0,0 +1,12 @@
+package org.example.util;
+
+public class userServiceUtil {
+
+    public static String hashPassword(String plainPassword) {
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String plainPassword, String hashedPassword) {
+        return BCrypt.checkpw(plainPassword, hashedPassword);
+    }
+}
