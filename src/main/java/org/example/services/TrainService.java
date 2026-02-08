e6d51e5129eca0a63ba094d52af150219d20d55f
Shivam <shivamtamboli66@gmail.com>
2026-02-08 18:27:12 +0530

    user can search trains using helper method name validtrains

--- src/main/java/org/example/services/TrainService.java
+++ src/main/java/org/example/services/TrainService.java
@@ -1,4 +1,39 @@
 package org.example.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entity.Train;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class TrainService {
+
+    private List<Train> trainList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String TRAIN_DB_PATH = "src/main/resources/localDb/train.json";
+
+    public TrainService() throws IOException{
+        File trains = new File(TRAIN_DB_PATH);
+        trainList = objectMapper.readValue(trains, new TypeReference<List<Train>>() {
+        });
+    }
+
+    public List<Train> searchTrains(String source, String destination){
+        return trainList.stream().filter(train -> validTrain(train, source, destination)).collect(Collectors.toList());
+    }
+
+    private boolean validTrain(Train train, String source, String destination){
+        List<String> stationOrder = train.getStations();
+
+        int sourceIndex = stationOrder.indexOf(source.toLowerCase());
+        int desinationIndex = stationOrder.indexOf(destination.toLowerCase());
+
+        return sourceIndex !=-1 && desinationIndex != -1 && sourceIndex < desinationIndex;
+    }
 }
