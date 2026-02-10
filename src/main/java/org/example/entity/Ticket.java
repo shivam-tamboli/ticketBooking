0e85ee1b51fea07d9fefded68f3da87ebb392031
Shivam <shivamtamboli66@gmail.com>
2026-02-10 19:50:37 +0530

    add json annotations so entity and json file can match the data

--- src/main/java/org/example/entity/Ticket.java
+++ src/main/java/org/example/entity/Ticket.java
@@ -1,22 +1,23 @@
 package org.example.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Ticket {
 
     private String ticketId;
-
     private String userId;
-
     private String source;
-
     private String destination;
-
     private String dateOfTravel;
-
     private Train train;
 
-    public Ticket(){}
+    public Ticket() {}
 
-    public Ticket(String ticketId, String userId, String source, String destination, String dateOfTravel, Train train){
+    public Ticket(String ticketId, String userId, String source, String destination, String dateOfTravel, Train train) {
         this.ticketId = ticketId;
         this.userId = userId;
         this.source = source;
@@ -25,56 +26,58 @@ public class Ticket {
         this.train = train;
     }
 
-    public String getTicketInfo(){
-        return String.format("Ticket ID: %s belongs to User %s from %s to %s on %s", ticketId, userId, source, destination, dateOfTravel);
+    public String getTicketInfo() {
+        return String.format(
+                "Ticket ID: %s | From %s to %s | Date: %s | Train: %s",
+                ticketId, source, destination, dateOfTravel, train.getTrainId()
+        );
     }
 
-    public String getTicketId(){
+    public String getTicketId() {
         return ticketId;
     }
 
-    public void setTicketId(String ticketId){
-        this.ticketId = ticketId;
-    }
-
-    public String getSource(){
-        return source;
-    }
-
-    public void setSource(String source){
-        this.source = source;
-    }
-
-    public String getUserId(){
+    public String getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId){
-        this.userId = userId;
+    public String getSource() {
+        return source;
     }
 
-    public String getDestination(){
+    public String getDestination() {
         return destination;
     }
 
-    public void setDestination(String destination){
-        this.destination = destination;
-    }
-
-    public String getDateOfTravel(){
+    public String getDateOfTravel() {
         return dateOfTravel;
     }
 
-    public void setDateOfTravel(String dateOfTravel){
-        this.dateOfTravel = dateOfTravel;
-    }
-
-    public Train getTrain(){
+    public Train getTrain() {
         return train;
     }
 
-    public void setTrain(Train train){
-        this.train = train;
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
     }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setDateOfTravel(String dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
 }
--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,35 +1,40 @@
 package org.example.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class User {
 
+    private String name;
+    private String password;
+    private String hashedPassword;
+    private List<Ticket> ticketsBooked;
     private String userId;
 
-    private String name;
+    public User() {
+        this.ticketsBooked = new ArrayList<>();
+    }
 
-    private String password;
-
-    private String hashedPassword;
-
-    private List<Ticket> ticketsBooked;
-
-    public User(String userId, String name, String password, String hashedPassword, List<Ticket> ticketsBooked) {
-        this.userId = userId;
+    public User(String name, String password, String hashedPassword,
+                List<Ticket> ticketsBooked, String userId) {
         this.name = name;
         this.password = password;
         this.hashedPassword = hashedPassword;
-        this.ticketsBooked = ticketsBooked;
+        this.ticketsBooked = (ticketsBooked == null) ? new ArrayList<>() : ticketsBooked;
+        this.userId = userId;
     }
-
-    public User(){}
 
     public String getName() {
         return name;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
 
@@ -38,12 +43,19 @@ public class User {
     }
 
     public List<Ticket> getTicketsBooked() {
+        if (ticketsBooked == null) {
+            ticketsBooked = new ArrayList<>();
+        }
         return ticketsBooked;
     }
 
-    public void printTickets(){
-        for (int i = 0; i<ticketsBooked.size(); i++){
-            System.out.println(ticketsBooked.get(i).getTicketInfo());
+    public void printTickets() {
+        if (ticketsBooked == null || ticketsBooked.isEmpty()) {
+            System.out.println("No bookings found.");
+            return;
+        }
+        for (Ticket ticket : ticketsBooked) {
+            System.out.println(ticket.getTicketInfo());
         }
     }
 
